5b5692aa4433341105855a4700cafb727b301c0e
TGVM <tgvm96@gmail.com>
2024-09-11 14:12:20 -0300

    - part1 c

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,31 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Olá {props.nome}, você tem {props.idade} anos</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+
+const Exibir = ({ contador }) => <div>{contador}</div>
+
+const Botao = ({ onClick, texto }) => <button onClick={onClick}>{texto}</button>
 
 const App = () => {
-  const nome = 'Peter'
-  const idade = 10
+  const [ contador, setContador ] = useState(0)
+  const aumentarEmUm = () => setContador(contador + 1)
+  const diminuirEmUm = () => setContador(contador - 1) 
+  const zerarContador = () => setContador(0)
 
   return (
     <div>
-      <h1>Olá a todos!</h1>
-      <Hello nome='Maya' idade={26 + 10} />
-      <Hello nome={nome} idade={idade} />
+      <Exibir contador={contador}/>
+
+      <Botao
+        onClick={aumentarEmUm}
+        texto='mais+'
+      />
+      <Botao
+        onClick={zerarContador}
+        texto='zerar'
+      />     
+      <Botao
+        onClick={diminuirEmUm}
+        texto='menos-'
+      />
     </div>
   )
 }
