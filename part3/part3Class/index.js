d956d5c31c8bf9275dfe8dc844be7a27700b9378
TGVM <tgvm96@gmail.com>
2024-09-23 11:23:19 -0300

    part3b1

--- part3/part3Class/index.js
+++ part3/part3Class/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -74,7 +76,7 @@ const generateId = () => {
     response.json(note)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
